cc9bd79252d4b73cff55c02f8de088535450ddfd
Michael Oceguera <syntaxmike91@gmail.com>
2023-06-24 15:35:19 -0700

    Implement function for 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const removeNonWords = str.toLowerCase().replace(/[^\w\d]/g, "");
+    return removeNonWords.split("").reverse().join("") === removeNonWords;
 };
 
 // Do not edit below this line
