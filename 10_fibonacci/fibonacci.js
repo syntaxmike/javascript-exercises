82e815e8e560b1d395e48158ddc61682389068c2
Michael Oceguera <syntaxmike91@gmail.com>
2023-06-24 16:07:10 -0700

    Implement function for 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if(+count < 0){
+        return 'OOPS';
+    }
 
+    if(+count === 0){
+        return 0;
+    } else if (+count === 1 || +count === 2) {
+        return 1;
+    }
+    return fibonacci(+count - 1) + fibonacci(+count - 2);
 };
 
 // Do not edit below this line
