dfc294550a2158ab513726c9a5843cb777d15dd5
Michael Oceguera <syntaxmike91@gmail.com>
2023-06-24 16:46:09 -0700

    Implement function for 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const sortByAge = (a, b) => {
+    const getCurrentYear = new Date().getFullYear();
+    if(!a.yearOfDeath){
+        return (b.yearOfDeath - b.yearOfBirth) - (getCurrentYear - a.yearOfBirth);
+    } else if (!b.yearOfDeath) {
+        return (getCurrentYear - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    }
+
+    return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+}
+
+const findTheOldest = function(listOfPeople) {
+
+    return listOfPeople.sort(sortByAge)[0];
 
 };
 
